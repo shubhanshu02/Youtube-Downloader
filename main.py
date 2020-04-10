290def0914e09fac55eed43bc063b05a7ac78edc
hex-plex <56990337+hex-plex@users.noreply.github.com>
2020-04-10 13:42:24 +0530

    Can specify the resolution

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import sys
 from pytube import YouTube
-def main(url=""):
+def main(url="",res=None):
     if url=="":
         url = input("Enter the URL of the youtube video: ")
-    
+    if res!=None:
+        res=res[1:]
     try:
         yt = YouTube(url)
         print("\nTitle of Video:", yt.title)
-        stream = yt.streams.filter(progressive=True, file_extension='mp4')
+        if res!=None:
+            try:
+                stream = yt.streams.filter(progressive=True, file_extension='mp4',res=str(res))
+            except:
+                print("the specified resoulution is not found")
+        else:
+            stream = yt.streams.filter(progressive=True, file_extension='mp4')
         stream = stream.order_by('resolution')
         stream = stream.desc()
         stream = stream.first()
@@ -20,7 +27,12 @@ def main(url=""):
         print("TRY AGAIN LATER!")
 
 if  __name__=="__main__":
-    if len(sys.argv)>1:
+    if len(sys.argv)==3:
+        main(sys.argv[2],sys.argv[1])
+    elif len(sys.argv)==2:
         main(sys.argv[1])
-    else:
+    elif len(sys.argv)==1:
         main()
+    else :
+        print("Wrong arguement please input:")
+        print("python main.py -res url")
