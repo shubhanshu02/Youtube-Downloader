3fb07b13b81f0f813eda9eb5fcc8ce8ed1befa0f
hex-plex <56990337+hex-plex@users.noreply.github.com>
2020-04-10 12:59:42 +0530

    Helps in exception and in accessibilty

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
+import sys
 from pytube import YouTube
+main(url=""):
+    if url=="":
+        url = input("Enter the URL of the youtube video: ")
+    
+    try:
+        yt = YouTube(url)
+        print("\nTitle of Video:", yt.title)
+        stream = yt.streams.filter(progressive=True, file_extension='mp4')
+        stream = stream.order_by('resolution')
+        stream = stream.desc()
+        stream = stream.first()
+        print(stream)
+        stream = stream.download()
+        print("Done Downloading", stream)
+    
+    except:
+        print("\nConnection Error\n")
+        print("TRY AGAIN LATER!")
 
-url = input("Enter the URL of the youtube video: ")
-
-try:
-    yt = YouTube(url)
-    print("\nTitle of Video:", yt.title)
-except:
-    print("\nConnection Error\n")
-
-stream = yt.streams.filter(progressive=True, file_extension='mp4')
-stream = stream.order_by('resolution')
-stream = stream.desc()
-stream = stream.first()
-print(stream)
-stream = stream.download()
-
-print("Done Downloading", stream)
-
+if  __name__=="__main__":
+    main(sys[1])
