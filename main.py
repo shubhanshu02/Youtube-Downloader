97ada308ed218dd2f3088d2ccf9728287bc20c1e
shubhanshu02 <shubhanshu.saxena2002@gmail.com>
2020-04-15 13:44:44 +0530

    Error Handling for GetYoutube

--- main.py
+++ main.py
@@ -1,32 +1,51 @@
 import sys
+import pytube
 from pytube import YouTube
+import urllib
+
+def getRequest(url: str):
+    try:
+        yt = YouTube(url)
+        print("\nTitle of Video:", yt.title)
+    except urllib.error.URLError:
+        print("\nNo Internet. Please make sure you have an active internet connection.")
+        exit()
+    except pytube.exceptions.RegexMatchError:
+        print("\nInvalid address for youtube video. ")
+        exit()
+    except:
+        print("\nSome unknown error occured. Please report it at https://github.com/shubhanshu02/Youtube-Downloader/issues")
+        exit()
+    return yt
+
+
 def main(url="",res=None):
     if url=="":
         url = input("Enter the URL of the youtube video: ")
+    yt = getRequest(url)
+
     if res != None:
         res=res[1:]
     else:
         res=str(input("Specify the resolution: "))
-    try:
-        yt = YouTube(url)
-        print("\nTitle of Video:", yt.title)
-        if res!=None:
-            try:
-                stream = yt.streams.filter(progressive=True, file_extension='mp4',res=str(res))
-            except:
-                print("the specified resoulution is not found")
-        else:
-            stream = yt.streams.filter(progressive=True, file_extension='mp4')
-        stream = stream.order_by('resolution')
-        stream = stream.desc()
-        stream = stream.first()
-        print(stream)
-        stream = stream.download()
-        print("Done Downloading", stream)
-
-    except:
-        print("\nConnection Error\n")
-        print("TRY AGAIN LATER!")
+        
+    if res!=None and len(res)!=0:
+        try:
+            stream = yt.streams.filter(progressive=True, file_extension='mp4',res=str(res))
+        except:
+            print("Some Error Occured")
+        finally:
+            if (stream == None):
+                print("Resolution Not Found")
+                exit()
+    else:
+        stream = yt.streams.filter(progressive=True, file_extension='mp4')
+    stream = stream.order_by('resolution')
+    stream = stream.desc()
+    stream = stream.first()
+    print(stream)
+    stream = stream.download()
+    print("Done Downloading", stream)
 
 if  __name__=="__main__":
     if len(sys.argv)==3:
